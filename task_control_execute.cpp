f2dc3277497829e638f835d1fd6edf942c3ec618
OkitaAqukinn <379183143@qq.com>
2024-04-11 21:35:01 +0800

    fix: 修复了传入字符串无法识别写入牌堆的问题

--- task_control_execute.cpp
+++ task_control_execute.cpp
@@ -3,9 +3,7 @@
 #include "thread_safe_queue.h"
 
 threadsafe_queue<cards_receive_data_t, 1> cards_send_data_queue;
-
 TaskControlExecute::TaskControlExecute() {}
-
 void TaskControlExecute::update(const cards_receive_data_t &data) {
     std::cout << "TaskControlExecute receive subCmd: " << data.subCmd
               << std::endl;
@@ -15,36 +13,36 @@ void TaskControlExecute::update(const cards_receive_data_t &data) {
         } break;
         case SubCmdType::kDeleteCards: {
             if (data.switch_type ==
-                static_cast<uint8_t>(SwitchCardType::kSwitchByIndex)) {
+                static_cast<int>(SwitchCardType::kSwitchByIndex)) {
                 del(data.cards_index, data.operate_cards);
-                std::cout << "result: "
-                          << getCalculatorPairProbability(data.cards_index)
-                          << std::endl;
+                // std::cout << "result: "
+                //           << getCalculatorPairProbability(data.cards_index)
+                //           << std::endl;
             } else if (data.switch_type ==
-                       static_cast<uint8_t>(SwitchCardType::kSwitchById)) {
+                       static_cast<int>(SwitchCardType::kSwitchById)) {
                 del(data.cards_id, data.operate_cards);
-                std::cout << "result: "
-                          << getCalculatorPairProbability(data.cards_id)
-                          << std::endl;
+                // std::cout << "result: "
+                //           << getCalculatorPairProbability(data.cards_id)
+                //           << std::endl;
             }
         } break;
         case SubCmdType::kModifyCards: {
             if (data.switch_type ==
-                static_cast<uint8_t>(SwitchCardType::kSwitchByIndex)) {
+                static_cast<int>(SwitchCardType::kSwitchByIndex)) {
                 modify(data.cards_index, data.operate_cards);
             } else if (data.switch_type ==
-                       static_cast<uint8_t>(SwitchCardType::kSwitchById)) {
+                       static_cast<int>(SwitchCardType::kSwitchById)) {
                 modify(data.cards_id, data.operate_cards);
             }
         } break;
         case SubCmdType::kLoadCards: {
             cards_receive_data_t send_data = data;
             if (data.switch_type ==
-                static_cast<uint8_t>(SwitchCardType::kSwitchByIndex)) {
+                static_cast<int>(SwitchCardType::kSwitchByIndex)) {
                 send_data.operate_cards =
                     load(data.cards_index).getCardsBase().getCards();
             } else if (data.switch_type ==
-                       static_cast<uint8_t>(SwitchCardType::kSwitchById)) {
+                       static_cast<int>(SwitchCardType::kSwitchById)) {
                 send_data.operate_cards =
                     load(data.cards_id).getCardsBase().getCards();
             }
@@ -58,17 +56,16 @@ void TaskControlExecute::update(const cards_receive_data_t &data) {
         }
     }
 }
-
-bool TaskControlExecute::checkIfIndexValid(uint8_t index) {
+bool TaskControlExecute::checkIfIndexValid(int index) {
     std::vector<CardsEventCalculator>::iterator it = cards_calculator_.begin();
     for (it; it != cards_calculator_.end(); it++) {
         if (it->getCardsIndex() == index) {
             return true;
         }
     }
+    std::cout << "Check index failed: " << index << std::endl;
     return false;
 }
-
 bool TaskControlExecute::checkIfIdValid(std::string id) {
     std::vector<CardsEventCalculator>::iterator it = cards_calculator_.begin();
     for (it; it != cards_calculator_.end(); it++) {
@@ -76,13 +73,12 @@ bool TaskControlExecute::checkIfIdValid(std::string id) {
             return true;
         }
     }
+    std::cout << "Check id failed: " << id << std::endl;
     return false;
 }
-
-void TaskControlExecute::add(uint8_t index, std::string id,
-                             const std::vector<uint8_t> &cards) {}
-
-void TaskControlExecute::del(uint8_t index, const std::vector<uint8_t> &cards) {
+void TaskControlExecute::add(int index, std::string id,
+                             const std::vector<int> &cards) {}
+void TaskControlExecute::del(int index, const std::vector<int> &cards) {
     if (!checkIfIndexValid(index)) {
         addCalculator(index);
     }
@@ -95,9 +91,7 @@ void TaskControlExecute::del(uint8_t index, const std::vector<uint8_t> &cards) {
         }
     }
 }
-
-void TaskControlExecute::del(std::string id,
-                             const std::vector<uint8_t> &cards) {
+void TaskControlExecute::del(std::string id, const std::vector<int> &cards) {
     if (!checkIfIdValid(id)) {
         addCalculator(id);
     }
@@ -110,13 +104,10 @@ void TaskControlExecute::del(std::string id,
         }
     }
 }
-
-void TaskControlExecute::modify(uint8_t index,
-                                const std::vector<uint8_t> &cards) {}
-void TaskControlExecute::modify(std::string id,
-                                const std::vector<uint8_t> &cards) {}
-
-CardsEventCalculator TaskControlExecute::load(uint8_t index) {
+void TaskControlExecute::modify(int index, const std::vector<int> &cards) {}
+void TaskControlExecute::modify(std::string id, const std::vector<int> &cards) {
+}
+CardsEventCalculator TaskControlExecute::load(int index) {
     std::vector<CardsEventCalculator>::iterator it = cards_calculator_.begin();
     for (it; it != cards_calculator_.end(); it++) {
         if (it->getCardsIndex() == index) {
@@ -126,7 +117,6 @@ CardsEventCalculator TaskControlExecute::load(uint8_t index) {
     std::cout << "del failed, index: " << index << std::endl;
     return CardsEventCalculator(0, 255, "-1");
 }
-
 CardsEventCalculator TaskControlExecute::load(std::string id) {
     std::vector<CardsEventCalculator>::iterator it = cards_calculator_.begin();
     for (it; it != cards_calculator_.end(); it++) {
@@ -137,26 +127,24 @@ CardsEventCalculator TaskControlExecute::load(std::string id) {
     std::cout << "del failed, id: " << id << std::endl;
     return CardsEventCalculator(0, 255, "-1");
 }
-
-void TaskControlExecute::addCalculator(uint8_t index) {
+void TaskControlExecute::addCalculator(int index) {
     std::shared_ptr<CardsEventCalculator> tmp_calc_ptr =
         std::make_shared<CardsEventCalculator>(MAX_CARD_SUIT_NUM, index, "0");
     cards_calculator_.push_back(*tmp_calc_ptr);
+    std::cout << index << " index already push back to vector" << std::endl;
 }
-
 void TaskControlExecute::addCalculator(std::string id) {
     std::shared_ptr<CardsEventCalculator> tmp_calc_ptr =
         std::make_shared<CardsEventCalculator>(MAX_CARD_SUIT_NUM, 0, id);
     cards_calculator_.push_back(*tmp_calc_ptr);
+    std::cout << id << " id already push back to vector" << std::endl;
 }
-
-void TaskControlExecute::addCalculator(uint8_t index, std::string id) {
+void TaskControlExecute::addCalculator(int index, std::string id) {
     std::shared_ptr<CardsEventCalculator> tmp_calc_ptr =
         std::make_shared<CardsEventCalculator>(MAX_CARD_SUIT_NUM, index, id);
     cards_calculator_.push_back(*tmp_calc_ptr);
 }
-
-double TaskControlExecute::getCalculatorPairProbability(uint8_t index) {
+double TaskControlExecute::getCalculatorPairProbability(int index) {
     return load(index).nextPairProbabilityCalc();
 }
 double TaskControlExecute::getCalculatorPairProbability(std::string id) {
--- task_control_execute.h
+++ task_control_execute.h
@@ -1,6 +1,5 @@
 #ifndef TASK_CONTROL_EXECUTE_H
 #define TASK_CONTROL_EXECUTE_H
-
 #include <algorithm>
 #include <iostream>
 #include <string>
@@ -9,14 +8,12 @@
 #include "calculator.h"
 
 #define MAX_CARD_SUIT_NUM (8)
-
-enum class SwitchCardType : uint8_t {
+enum class SwitchCardType : int {
     kUnknowType = 0,
     kSwitchByIndex = 1,
     kSwitchById = 2
 };
-
-enum class SubCmdType : uint8_t {
+enum class SubCmdType : int {
     kNoneType = 0,
     kAddCards = 1,
     kDeleteCards = 2,
@@ -24,15 +21,13 @@ enum class SubCmdType : uint8_t {
     kLoadCards = 4,
     kResetAllCards = 5
 };
-
 typedef struct {
-    uint8_t subCmd;
-    uint8_t switch_type;
-    uint8_t cards_index;
+    int subCmd;
+    int switch_type;
+    int cards_index;
     std::string cards_id;
-    std::vector<uint8_t> operate_cards;
+    std::vector<int> operate_cards;
 } cards_receive_data_t;
-
 class TaskControlExecute {
    public:
     TaskControlExecute();
@@ -47,24 +42,23 @@ class TaskControlExecute {
     void update(const cards_receive_data_t &data);
 
    private:
-    bool checkIfIndexValid(uint8_t index);
+    bool checkIfIndexValid(int index);
     bool checkIfIdValid(std::string id);
-    void add(uint8_t index, std::string id, const std::vector<uint8_t> &cards);
-    void del(uint8_t index, const std::vector<uint8_t> &cards);
-    void del(std::string id, const std::vector<uint8_t> &cards);
-    void modify(uint8_t index, const std::vector<uint8_t> &cards);
-    void modify(std::string id, const std::vector<uint8_t> &cards);
-    CardsEventCalculator load(uint8_t index);
+    void add(int index, std::string id, const std::vector<int> &cards);
+    void del(int index, const std::vector<int> &cards);
+    void del(std::string id, const std::vector<int> &cards);
+    void modify(int index, const std::vector<int> &cards);
+    void modify(std::string id, const std::vector<int> &cards);
+    CardsEventCalculator load(int index);
     CardsEventCalculator load(std::string id);
-    void addCalculator(uint8_t index);
+    void addCalculator(int index);
     void addCalculator(std::string id);
-    void addCalculator(uint8_t index, std::string id);
+    void addCalculator(int index, std::string id);
     void resetAllCalculator() { cards_calculator_.clear(); };
-    double getCalculatorPairProbability(uint8_t index);
+    double getCalculatorPairProbability(int index);
     double getCalculatorPairProbability(std::string id);
 
    private:
     std::vector<CardsEventCalculator> cards_calculator_;
 };
-
 #endif  // TASK_CONTROL_EXECUTE_H
