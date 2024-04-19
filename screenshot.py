c5671e8371d245b9fdfaa78ec8062cfdba9c298d
yangziqi <ziqi.yang@agilex.ai>
2024-04-19 11:10:57 +0800

    feat: 新增自动循环截图的py脚本
    fix: 修改一下readme

---
+++ screenshot.py
@@ -0,0 +1,22 @@
+import os
+import pyautogui
+
+fp = r'C:\\Template\\CardsRecordCalculate\\cache' #指定文件夹
+ 
+# 创建文件夹
+if not os.path.exists(fp):
+    os.makedirs(fp)
+ 
+#开始截屏
+i = 0
+while True:
+    i += 1
+    b = str(i)
+    pyautogui.FAILSAFE = True
+    pyautogui.PAUSE = 1.0
+    recording = pyautogui.screenshot()  #截屏
+    fn = b + '.jpg'
+    print("screenshot success" + fn)
+    file_path = os.path.join(fp, fn)    #截屏截到的图片保存地址，这里保存到了刚才创建的文件夹中
+    recording.save(file_path)           #保存图片
+ 
